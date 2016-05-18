a1f23a7a74781c038d1f1716745c8d496d965ab6
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-05-18 13:39:15 +0200

    add skeleton

---
+++ README.rst
@@ -0,0 +1,11 @@
+seed_jsbox_utils
+=============
+
+|travis|_
+
+JS Sandbox specfic utils.
+
+::
+
+    $ npm install
+    $ npm test
---
+++ test/test_utils.js
@@ -0,0 +1,15 @@
+
+var Utils = require("../lib");
+var assert = require('assert');
+var moment = require('moment');
+
+
+describe("Testing Utils Functions", function() {
+
+    describe("", function() {
+        it("", function() {
+
+        });
+    });
+
+});
