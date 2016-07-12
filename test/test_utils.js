5418ee4d76c56a20661d502b0253bf7ac868e16f
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-07-12 20:20:32 +0200

    add more utils function (generic ones)

--- test/test_utils.js
+++ test/test_utils.js
@@ -18,9 +18,38 @@ var $ = App.$;
 var service = require('../lib');
 var utils = require('../lib/utils');
 
-describe("Testing utils Functions", function() {
+describe("Testing utils functions", function() {
 
-    describe("Tests is_valid_msisdn function", function() {
+    describe("check_valid_number", function() {
+        it("only numbers is valid", function() {
+            assert(utils.check_valid_number("012345"), true);
+        });
+        it("any letters invalidates check", function() {
+            assert.ifError(utils.check_valid_number("012abc345"));
+        });
+        it("any other characters invalidates check", function() {
+            assert.equal(utils.check_valid_number("-123456"), false);
+            assert.equal(utils.check_valid_number("123-456"), false);
+            assert.equal(utils.check_valid_number("1234&56"), false);
+            assert.equal(utils.check_valid_number("1%234#56"), false);
+        });
+    });
+
+    describe("double_digit_number", function() {
+        it("single digit numbers should be prepended with '0'", function() {
+            assert.deepEqual(utils.double_digit_number(1), '01');
+            assert.deepEqual(utils.double_digit_number(4), '04');
+        });
+        it("double digits number should stay unchanged", function() {
+            assert.deepEqual(utils.double_digit_number(10), '10');
+            assert.deepEqual(utils.double_digit_number(95), '95');
+        });
+        it("doens't handle negative numbers; scrambled output", function() {
+            assert.deepEqual(utils.double_digit_number(-1), '0-1');
+        });
+    });
+
+    describe("is_valid_msisdn function", function() {
         it("should not validate if passed a number that doesn't start with '0'", function() {
             assert.equal(utils.is_valid_msisdn("12345"), false);
         });
@@ -34,25 +63,192 @@ describe("Testing utils Functions", function() {
         });
     });
 
-    describe("Tests get_today function", function() {
+    describe("normalize_msisdn(raw, country_code)", function() {
+        it("return raw number unchanged if shortcode", function() {
+            assert.deepEqual(utils.normalize_msisdn("0123", "123"), "0123");
+        });
+        it.skip("remove chars that are not numbers or + from raw nuumber", function() {
+            assert.deepEqual(utils.normalize_msisdn("012abc345", "123"), "+12312345");
+        });
+        it("starts with '00'; replace with '+', don't prepend country_code", function() {
+            assert.deepEqual(utils.normalize_msisdn("0012345", "123"), "+12345");
+        });
+        it("starts with '0'; replace with '+' + country_code", function() {
+            assert.deepEqual(utils.normalize_msisdn("012345", "123"), "+12312345");
+        });
+        it("starts with '+'; return raw number as is", function() {
+            assert.deepEqual(utils.normalize_msisdn("+12312345", "123"), "+12312345");
+        });
+        it("if raw number's length equals country_code, prepend '+'", function() {
+            assert.deepEqual(utils.normalize_msisdn("123456", "123456"), "+123456");
+        });
+    });
+
+    describe("get_timestamp", function() {
         it("when date passed in, return the same as moment object", function() {
-            assert.deepEqual(utils.get_today(testing_config).format("YYYY-MM-DD"),
+            assert.deepEqual(utils.get_timestamp("YYYY-MM-DD-HH-mm-ss"),
+                new moment().format("YYYY-MM-DD-HH-mm-ss"));
+        });
+        it("no date passed, return current moment object", function() {
+            assert.deepEqual(utils.get_timestamp(),
+                new moment().format("YYYYMMDDHHmmss"));
+        });
+    });
+
+    describe("get_today", function() {
+        it("when date passed in, return the same as moment object", function() {
+            assert.deepEqual(utils.get_today("2016-05-19").format("YYYY-MM-DD"),
+                moment("2016-05-19").format("YYYY-MM-DD"));
+        });
+        it("when date (config) passed in, return the same as moment object", function() {
+            assert.deepEqual(utils.get_today(testing_config.testing_today, "YYYY-MM-DD").format("YYYY-MM-DD"),
                 moment("2016-05-23").format("YYYY-MM-DD"));
         });
         it("no date passed, return current moment object", function() {
-            assert.deepEqual(utils.get_today(live_config).format("YYYY-MM-DD"),
+            assert.deepEqual(utils.get_today().format("YYYY-MM-DD"),
                 new moment().format("YYYY-MM-DD"));
         });
     });
 
-    describe("Tests get_january function", function() {
-        it("get 1st jan moment date of current year", function() {
-            assert.deepEqual(utils.get_january(testing_config).format("YYYY-MM-DD"),
+    describe("get_january", function() {
+        it("get 1st jan moment date of any given year (test date)", function() {
+            assert.deepEqual(utils.get_january("2016-05-19").format("YYYY-MM-DD"),
                 moment("2016-01-01").format("YYYY-MM-DD"));
+            assert.deepEqual(utils.get_january("2013-08-24").format("YYYY-MM-DD"),
+                moment("2013-01-01").format("YYYY-MM-DD"));
+        });
+        it("get 1st jan moment date of current year", function() {
+            assert.deepEqual(utils.get_january(testing_config.testing_today, "YYYY-MM-DD").format("YYYY-MM-DD"),
+                moment("2016-01-01").format("YYYY-MM-DD"));
+        });
+        it("get 1st jan moment date of current year", function() {
+            assert.deepEqual(utils.get_january().format("YYYY-MM-DD"),
+                new moment().format("YYYY-01-01"));
         });
     });
 
-    describe("Tests make_month_choices function", function() {
+    describe("is_valid_date", function() {
+        it("returns true for valid YYYY-MM-DD dates", function() {
+            assert(utils.is_valid_date("2016-05-19", "YYYY-MM-DD"), true);
+        });
+        it("returns true for valid YYYY/MM/DD dates", function() {
+            assert(utils.is_valid_date("2016/05/19", "YYYY/MM/DD"), true);
+        });
+        it("returns true for valid YYYY/DD/MM dates", function() {
+            assert(utils.is_valid_date("2016/19/05", "YYYY/DD/MM"), true);
+        });
+        it("returns true for valid DD MMMM 'YY dates", function() {
+            assert(utils.is_valid_date("05 May '16", "DD MMMM 'YY"), true);
+        });
+        it("returns false for valid date specified with unmatching format", function() {
+            assert.ifError(utils.is_valid_date("2016-05-19", "YYYY/MM/DD"));
+        });
+        it("returns false for invalid date", function() {
+            // invalid day
+            assert.equal(utils.is_valid_date("2015-05-32", "YYYY-MM-DD"), false);
+            // invalid day - leap year example
+            assert.equal(utils.is_valid_date("2015-02-29", "YYYY-MM-DD"), false);
+            // invalid month
+            assert.equal(utils.is_valid_date("2015-13-19", "YYYY-MM-DD"), false);
+            // invalid year
+            assert.equal(utils.is_valid_date("20151-05-19", "YYYY-MM-DD"), false);
+        });
+    });
+
+    describe("is_valid_year", function() {
+        it("valid; year within bounds", function() {
+            assert(utils.is_valid_year("2016", "1990", "2030"), true);
+            assert(utils.is_valid_year("2016", "2015", "2017"), true);
+            assert(utils.is_valid_year("2016", "2016", "2017"), true);
+            assert(utils.is_valid_year("2016", "2015", "2016"), true);
+            assert(utils.is_valid_year("2016", "2016", "2016"), true);
+        });
+        it("invalid; year outside bounds", function() {
+            assert.equal(utils.is_valid_year("2016", "2010", "2015"), false);
+            assert.equal(utils.is_valid_year("2016", "2017", "2020"), false);
+        });
+    });
+
+    describe("is_valid_day_of_month", function() {
+        it("valid day of the month", function() {
+            assert(utils.is_valid_day_of_month("1"), true);
+            assert(utils.is_valid_day_of_month("5"), true);
+            assert(utils.is_valid_day_of_month("15"), true);
+            assert(utils.is_valid_day_of_month("28"), true);
+            assert(utils.is_valid_day_of_month("30"), true);
+            assert(utils.is_valid_day_of_month("31"), true);
+        });
+        it("invalid day of the month", function() {
+            assert.equal(utils.is_valid_day_of_month("0"), false);
+            assert.equal(utils.is_valid_day_of_month("32"), false);
+        });
+    });
+
+    describe("check_valid_alpha", function() {
+        it("valid alphabetical", function() {
+            assert(utils.check_valid_alpha("abc"));
+            assert(utils.check_valid_alpha("JohnDeere"));
+        });
+        it("invalid alphabetical", function() {
+            assert.equal(utils.check_valid_alpha(""), false);
+            assert.equal(utils.check_valid_alpha(" "), false);
+            assert.equal(utils.check_valid_alpha("John Deere"), false);
+            assert.equal(utils.check_valid_alpha("A123"), false);
+            assert.equal(utils.check_valid_alpha("A#1"), false);
+        });
+    });
+
+    describe("is_alpha_numeric_only", function() {
+        it("valid alpha-numerics", function() {
+            assert(utils.is_alpha_numeric_only("John"));
+            assert(utils.is_alpha_numeric_only("John123"));
+            assert(utils.is_alpha_numeric_only("J1o2h3n"));
+        });
+        it("invalid alpha-numerics", function() {
+            assert.equal(utils.is_alpha_numeric_only(" 123"), false);
+            assert.equal(utils.is_alpha_numeric_only("Jo h n"), false);
+            assert.equal(utils.is_alpha_numeric_only("J1o#hn?"), false);
+        });
+    });
+
+    describe("is_valid_name", function() {
+        it("valid name", function() {
+            assert(utils.is_valid_name("John", 1, 5));
+            assert(utils.is_valid_name("Ba Ki-moon", 1, 15));
+            assert(utils.is_valid_name("-Jo-hn", 1, 10));
+        });
+        it("invalid name", function() {
+            assert.equal(utils.is_valid_name("123", 1, 5), false);
+            assert.equal(utils.is_valid_name("John", 1, 3), false);
+            assert.equal(utils.is_valid_name("John?", 1, 5), false);
+        });
+    });
+
+    describe("get_clean_first_word", function() {
+        it("should get and capitalise first word", function() {
+            assert.deepEqual(utils.get_clean_first_word("Only"), "ONLY");
+            assert.deepEqual(utils.get_clean_first_word("Once there was..."), "ONCE");
+            assert.deepEqual(utils.get_clean_first_word("Stop the noise"), "STOP");
+        });
+        it("should get clean first word if contains non-letters/numbers", function() {
+            assert.deepEqual(utils.get_clean_first_word("O$ne Two T3ree"), "ONE");
+            assert.deepEqual(utils.get_clean_first_word("O$1ne T2wo Th3ree"), "O1NE");
+        });
+    });
+
+    describe("is_true", function() {
+        it("valid", function() {
+            assert(utils.is_true(true));
+            assert(utils.is_true("true"));
+        });
+        it("invalid", function() {
+            assert.equal(utils.is_true(undefined), false);
+            assert.equal(utils.is_true("True"), false);
+            assert.equal(utils.is_true(false), false);
+        });
+    });
+
+    describe("make_month_choices function", function() {
         it('should return a Choice array of correct size - forward in same year', function() {
             // test data
             var testDate = moment("2015-04-26");
@@ -146,7 +342,7 @@ describe("Testing utils Functions", function() {
     var testing_config = {
         "testing_today": "2016-05-23"
     };
-    var live_config = {};
+    // var live_config = {};
 
     var app;
     var tester;
@@ -320,7 +516,7 @@ describe("Testing utils Functions", function() {
             ;
     });
 
-    describe("Tests check_fixtures_used function", function() {
+    describe("check_fixtures_used function", function() {
         it("no fixtures used", function() {
             return tester
                 .setup.user.addr('08212345678')
@@ -369,7 +565,7 @@ describe("Testing utils Functions", function() {
         });
     });
 
-    describe("Tests timed_out function", function() {
+    describe("timed_out function", function() {
         it("no time-out redirect; move on to next state", function() {
             return tester
                 .setup.user.addr('08212345678')
@@ -442,7 +638,7 @@ describe("Testing utils Functions", function() {
         });
     });
 
-    describe("Tests timeout_redirect function", function() {
+    describe("timeout_redirect function", function() {
         it("time-out redirect; from state_four to state_one)", function() {
             return tester
                 .setup.user.addr('08212345678')
@@ -459,7 +655,7 @@ describe("Testing utils Functions", function() {
         });
     });
 
-    describe("Tests log_service_call function", function() {
+    describe("log_service_call function", function() {
         it("logging of http GET request", function() {
             return tester
                 .setup.user.addr('08212345678')
