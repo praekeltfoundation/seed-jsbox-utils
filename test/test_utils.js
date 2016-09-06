e54a0add96397c093cb3d6a8c2c93fde31c02e04
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-09-06 09:37:05 +0200

    add more descriptive test comments/asserts

--- test/test_utils.js
+++ test/test_utils.js
@@ -1268,7 +1268,7 @@ describe("Testing app- and service call functions", function() {
             });
         });
         describe("Testing check_identity_subscribed function", function() {
-            it("expect 'true'", function() {
+            it("should return true if the identity has an active subscription to the messageset", function() {
                 return tester
                     .setup.user.addr('08212345678')
                     .check(function(api) {
@@ -1282,12 +1282,12 @@ describe("Testing app- and service call functions", function() {
                     })
                     .run();
             });
-            it("expect 'false' (no subscription to messageset)", function() {
+            it("should return false if the identity has no active subscription to messageset", function() {
                 return tester
                     .check(function(api) {
                         return sbm.check_identity_subscribed("cb245673-aa41-4302-ac47-00000000001", "nurseconnect")
                             .then(function(is_subscribed) {
-                                assert.ifError(is_subscribed);
+                                assert.equal(is_subscribed, false);
                             });
                     })
                     .check(function(api) {
@@ -1295,13 +1295,13 @@ describe("Testing app- and service call functions", function() {
                     })
                     .run();
             });
-            it("expect 'false' (no active subscriptions)", function() {
+            it("should return false if the identity has no active subscriptions", function() {
                 return tester
                     .setup.user.addr('08212345678')
                     .check(function(api) {
                         return sbm.check_identity_subscribed("cb245673-aa41-4302-ac47-00000000002", "nurseconnect")
                             .then(function(is_subscribed) {
-                                assert.ifError(is_subscribed);
+                                assert.equal(is_subscribed, false);
                             });
                     })
                     .check(function(api) {
