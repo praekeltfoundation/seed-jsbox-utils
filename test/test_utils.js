8f654b5754bedcc1253ac0659fc1d200f7c751a7
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-09-22 07:59:54 +0200

    remove duplicate test

--- test/test_utils.js
+++ test/test_utils.js
@@ -130,10 +130,6 @@ describe("Testing utils functions", function() {
         it("no date passed, return current moment object", function() {
             assert.deepEqual(utils.get_moment_date().format(), new moment().format());
         });
-        it("when date passed in, return corresponding moment object", function() {
-            var date_to_test = config.testing_today.split(" ")[0]; // get only "YYYY-MM-DD" part of testing_today
-            assert.deepEqual(utils.get_moment_date(date_to_test).format("YYYY-MM-DD"), "2016-05-23");
-        });
         it("when date passed in (not matching default date format), return corresponding moment object", function() {
             assert.deepEqual(utils.get_moment_date(config.testing_today).format("YYYY-MM-DD"), "2016-05-23");
         });
