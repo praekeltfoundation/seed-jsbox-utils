7803d569f8edb03cd66ad8e37935f468b46a23fb
NicolaasDieBaas <nicki@westerncapelabs.com>
2016-09-22 07:59:01 +0200

    remove explicit date validation

--- test/test_utils.js
+++ test/test_utils.js
@@ -134,11 +134,8 @@ describe("Testing utils functions", function() {
             var date_to_test = config.testing_today.split(" ")[0]; // get only "YYYY-MM-DD" part of testing_today
             assert.deepEqual(utils.get_moment_date(date_to_test).format("YYYY-MM-DD"), "2016-05-23");
         });
-        it("when date passed in, return corresponding moment object", function() {
-            // if testing_today is passed in alone and not in the default format "YYYY-MM-DD"
-            // the today's date will be returned
-            assert.deepEqual(utils.get_moment_date(config.testing_today).format("YYYY-MM-DD"),
-                new moment().format("YYYY-MM-DD"));
+        it("when date passed in (not matching default date format), return corresponding moment object", function() {
+            assert.deepEqual(utils.get_moment_date(config.testing_today).format("YYYY-MM-DD"), "2016-05-23");
         });
         it("when date & format passed in, return corresponding moment object", function() {
             assert.deepEqual(utils.get_moment_date(config.testing_today, "YYYY-MM-DD hh:mm:ss").format("YYYY-MM-DD hh:mm:ss"),
